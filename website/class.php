df01c05288e4be491a117f5323db10a2e491f30a
Alex B <arby36@users.noreply.github.com>
2016-10-08 16:55:51 -0400

    Add files via upload

---
+++ website/class.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Planner - Classrooms</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <div class="header">
+        <div class="tenpad">
+            <h1 class="head"><span style="color: white;">2Goo > </span>Planner</h1>
+        </div>
+    </div>
+    <?php 
+    $code = $_POST["code"];
+    $myfile = fopen("classroom" . $code . ".txt", "w")
+    fclose($myfile);
+    echo "Classroom " . $code . " was created.";
+    ?>
+</body>
+</html>
